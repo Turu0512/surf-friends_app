ea7463b1303b6bbc68c2569743db0c9b274cfeaa
tsurup <tsurup@pepabo.com>
2023-03-27 06:58:44 +0900

    テストファイルでfactorybotのUseモデルを利用

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "Userを新規作成する" do
-    user = User.new(
-      name: "hoge",
-    )
+    user = FactoryBot.build(:user)
     expect(user).to be_valid
   end
 end
